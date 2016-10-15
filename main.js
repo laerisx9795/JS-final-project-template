e2f795897c63ce443a1dbe21a58d5bb3eb239395
laerisx9795 <laerisx9795@gmail.com>
2016-10-15 16:41:49 +0800

    Canvas2

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ bgiImg.src="images/map.png";
 var canvas = document.getElementByld("game-canvas");
 
 var ctx = canvas.getContext("2d");
+
+function draw(){
+  ctx.drawImage(bgImg,0,0);
+}
+
+draw();
