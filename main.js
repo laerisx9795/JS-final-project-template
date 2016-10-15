06340d82fa5cba33ff587aa7b1c387791caaa85a
laerisx9795 <laerisx9795@gmail.com>
2016-10-15 17:04:57 +0800

    krkt2

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ function draw(){
 //setTimeout(draw,1000);
 
 setInterval(draw,16);
+
+
+var chaImg = document.createElement("img2");
+
+chaImg.src="images/jason.gif";
+
+var canvas2 = document.getElementById("krkt");
+
+var ctx = canvas2.getContext("2d");
+
+function draw2(){
+  ctx.drawImage(chaImg,0,0);
+}
+
+//setTimeout(draw,1000);
+
+setInterval(draw2,16);
