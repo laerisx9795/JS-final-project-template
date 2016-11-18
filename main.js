df5c2fe7dd702e13a5894c79f208c24f906b3111
laerisx9795 <laerisx9795@gmail.com>
2016-11-19 07:55:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var bgImg = document.createElement("img");
-bgImg.src="images/map.png";
+bgImg.src="images/102101.png";
 
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
