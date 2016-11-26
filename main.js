86c3b2ca3cdbd8c23995eff817254471f1dae582
laerisx9795 <laerisx9795@gmail.com>
2016-11-26 18:16:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -313,7 +313,7 @@ function draw(){
   ctx.drawImage(btnImg,btn.x,btn.y,50,50);
   for(var z=0; z<Towers.length; z++){
     //ctx.drawImage{twrImg,tower.x,tower.y,32,32};
-    ctx.drawImage(twrImg,Towers[z].x,Towers[z].y,32,32);
+    //ctx.drawImage(twrImg,Towers[z].x,Towers[z].y,32,32);
     if(isBuilding){
       //ctx.drawImage(twrImg,cursor.x,cursor.y,32,32);
       ctx.drawImage(twrImg,Towers[z].x,Towers[z].y,32,32);
